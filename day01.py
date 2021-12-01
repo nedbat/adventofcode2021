479af0465fe902b6101db2f4838c2810d00b1f67
Ned Batchelder <ned@nedbatchelder.com>
2021-12-01 06:44:15 -0500

    day 1, nicer

--- day01.py
+++ day01.py
@@ -1,6 +1,7 @@
 # https://adventofcode.com/2021/day/1
 
 import collections
+import itertools
 
 with open("day01_input.txt") as f:
     depths = list(map(int, f))
@@ -28,3 +29,13 @@ def sliding(seq, width):
 windows = list(sliding(depths, 3))
 increases = sum(b > a for a, b in zip(windows, windows[1:]))
 print(f"Part 2: there are {increases} increases")
+
+# I don't like needing a list to count the increases, so:
+
+def increases(seq):
+    s1, s2 = itertools.tee(seq)
+    next(s2)
+    return sum(b > a for a, b in zip(s1, s2))
+
+print(f"part 1: {increases(depths)}")
+print(f"part 2: {increases(sliding(depths, 3))}")
