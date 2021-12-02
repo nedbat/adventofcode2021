71dd80cb083c66cbec2beb4a39c6cd451bf9c0f1
Ned Batchelder <ned@nedbatchelder.com>
2021-12-02 07:42:52 -0500

    day 2: part 2

--- day02.py
+++ day02.py
@@ -29,3 +29,27 @@ if __name__ == "__main__":
         hor, dep = navigate(f)
     print(f"part 1: {hor * dep}")
 
+def parse_line(line):
+    word, num = line.split()
+    return word, int(num)
+
+def navigate2(lines):
+    hor = dep = aim = 0
+    for line in lines:
+        match parse_line(line):
+            case ["forward", num]:
+                hor += num
+                dep += aim * num
+            case ["down", num]:
+                aim += num
+            case ["up", num]:
+                aim -= num
+    return hor, dep
+
+def test_navigate2():
+    assert navigate2(TEST) == (15, 60)
+
+if __name__ == "__main__":
+    with open("day02_input.txt") as f:
+        hor, dep = navigate2(f)
+    print(f"part 2: {hor * dep}")
