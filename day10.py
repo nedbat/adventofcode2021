64e86abda7b0d085ad0e44b49351a2af9e2a5f67
Ned Batchelder <ned@nedbatchelder.com>
2021-12-10 07:09:08 -0500

    day 10, part 2

--- day10.py
+++ day10.py
@@ -1,5 +1,6 @@
 # https://adventofcode.com/2021/day/10
 
+import statistics
 from pathlib import Path
 
 import pytest
@@ -7,9 +8,9 @@ import pytest
 PAIRS = "()[]{}<>"
 
 OPENS = dict(zip(PAIRS[1::2], PAIRS[::2]))
+CLOSES = dict(zip(PAIRS[::2], PAIRS[1::2]))
 
-def corrupted(s):
-    """Returns the first wrong char, or None if not corrupted"""
+def check_syntax(s):
     stack = []
     for c in s:
         if c in "([{<":
@@ -18,8 +19,13 @@ def corrupted(s):
             if stack[-1] == OPENS[c]:
                 stack.pop()
             else:
-                return c
-    return None
+                return c, stack
+    return None, stack
+
+def corrupted(s):
+    """Returns the first wrong char, or None if not corrupted"""
+    c, stack = check_syntax(s)
+    return c
 
 
 VALIDS = [
@@ -55,3 +61,42 @@ def test_part1():
 if __name__ == "__main__":
     ans = part1("day10_input.txt")
     print(f"part 1: {ans}")
+
+def completion(s):
+    c, stack = check_syntax(s)
+    if c is None and stack:
+        return "".join(CLOSES[c] for c in stack[::-1])
+    return None
+
+@pytest.mark.parametrize("s, comp", [
+    ("[({(<(())[]>[[{[]{<()<>>", "}}]])})]"),
+    ("[(()[<>])]({[<{<<[]>>(", ")}>]})"),
+    ("(((({<>}<{<{<>}{[]{[]{}", "}}>}>))))"),
+    ("{<[[]]>}<{[{[{[]{()[[[]", "]]}}]}]}>"),
+    ("<{([{{}}[<[[[<>{}]]]>[]]", "])}>"),
+])
+def test_completion_needed(s, comp):
+    assert completion(s) == comp
+
+@pytest.mark.parametrize("s", VALIDS)
+def test_completion_not_needed(s):
+    assert completion(s) is None
+
+def part2(fname):
+    scores = []
+    for line in Path(fname).open():
+        comp = completion(line.strip())
+        if comp:
+            score = 0
+            for c in comp:
+                score *= 5
+                score += " )]}>".find(c)
+            scores.append(score)
+    return statistics.median(scores)
+
+def test_part2():
+    assert part2("day10_sample.txt") == 288957
+
+if __name__ == "__main__":
+    ans = part2("day10_input.txt")
+    print(f"part 2: {ans}")
