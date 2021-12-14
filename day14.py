a74c12e47146008dad0a41dd515f91314c330e14
Ned Batchelder <ned@nedbatchelder.com>
2021-12-14 06:31:53 -0500

    day 14, step 2

--- day14.py
+++ day14.py
@@ -37,3 +37,44 @@ def test_part1():
 if __name__ == "__main__":
     ans = part1("day14_input.txt")
     print(f"part 1: {ans}")
+
+
+# Part 1 literally made each polymer. Part 2 can't, they get too long.
+
+def step2(polypairs, rules):
+    new = collections.Counter()
+    for pair, count in polypairs.items():
+        a, b = pair
+        middle = rules[pair]
+        new[a + middle] += count
+        new[middle + b] += count
+    return new
+
+def part2(fname):
+    polymer, rules = read_input(fname)
+    polypairs = collections.Counter()
+    # turn the polymer into a count of pairs
+    for a, b in itertools.pairwise(polymer):
+        polypairs[a + b] += 1
+    # run for 40 steps
+    for _ in range(40):
+        polypairs = step2(polypairs, rules)
+    # count individual elements: this double counts, except
+    # for the two elements at the ends.
+    elements = collections.Counter()
+    for (a, b), count in polypairs.items():
+        elements[a] += count
+        elements[b] += count
+    # get the ends
+    elements[polymer[0]] += 1
+    elements[polymer[-1]] += 1
+    census = elements.most_common()
+    return (census[0][1] - census[-1][1]) // 2
+
+def test_part2():
+    assert part2("day14_sample.txt") == 2188189693529
+
+if __name__ == "__main__":
+    ans = part2("day14_input.txt")
+    print(f"part 2: {ans}")
+
